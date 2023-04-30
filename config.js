a60ac3d400b9753dd026cc480ee01c50af697836
Wasiq Gilani <42010359+swgilani@users.noreply.github.com>
2023-04-30 17:33:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ const config = {
     orcestraPsetsUrl: "https://www.orcestra.ca/api/psets/canonical"
   },
   db: {
-    host: 'root',
-    port: 'root',
-    dbClusterName: 'development-cluster-ptdz3.azure.mongodb.net/orcestra-new'
+    host: '',
+    port: '',
+    dbClusterName: ''
 
   }
 };
